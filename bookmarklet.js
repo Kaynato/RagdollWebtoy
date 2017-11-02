4060c0907c80c966d1098b6b0002561a075bdf88
Zicheng Gao <kaynato@live.com>
2017-11-02 01:59:09 -0400

    http to https in bookmarklet script reference

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){var F=document.createElement('script');F.type="text/javascript";F.src="http://kaynato.github.io/RagdollWebtoy/webtoy-portable.js";document.getElementsByTagName("head")[0].appendChild(F);})();
+javascript:(function(){var F=document.createElement('script');F.type="text/javascript";F.src="https://kaynato.github.io/RagdollWebtoy/webtoy-portable.js";document.getElementsByTagName("head")[0].appendChild(F);})();
